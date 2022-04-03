b42d8d71d1317f024ca5761f22822a5c7e6ea1f9
agreakash24 <102405478+agreakash24@users.noreply.github.com>
2022-04-03 16:38:47 +0530

    Create quitregist.c

---
+++ 03_Implementation/src/quitregist.c
@@ -0,0 +1,13 @@
+#include"stdio.h"
+#include "registration.h"
+
+// function to store user information
+int quitregist(){
+    pd[1].persone_age=123;
+    pd[1].aadhar_card_num=123;
+    pd[1].mobile_number=123;
+    pd[1].unique_code=123;
+    pd[1].different_vac=1;
+    pd[1].vac_take=1;
+    return 0;
+}
